4a6d3803b6be3f2a555d439a1145caf36a533d44
luizrocha <luiz.rocha9900@gmail.com>
2025-05-20 18:41:54 -0300

    painel de municípios prioritários

--- scripts/soja_data.R
+++ scripts/soja_data.R
@@ -62,18 +62,37 @@
  
     soja_data <- 
       df %>%
-      filter(ano > 2013 & ano < 2021, produto == "Soja (em grão)", area_plantada > 5000) %>%
-      arrange(id_municipio)
+      filter(produto == "Soja (em grão)", area_plantada > 5000) %>%
       select(ano, id_municipio, valor_producao, quantidade_produzida) %>% 
       mutate(p_medio = log(valor_producao / quantidade_produzida)) 
+      
         
-    mun_prio <- 
+    mun_prio_aux <- 
       municipios_formatados %>% 
       rename(nome = NOME) %>%
-      filter(is.na(saida)) %>%
-      filter(entrada < 2017) %>%
-      select(nome) %>%
-      mutate(Prio = 1)
+      select(nome, entrada, saida) 
+    
+      mun_prio <- 
+        left_join(expand_grid(nome = mun_prio_aux$nome, ano = seq(2008,2022, 1)), mun_prio_aux) %>% 
+        mutate(
+          across(
+            ends_with("a"),
+            as.double),
+          Prio = case_when(
+            ano < entrada ~ 0,
+            ano >= saida ~ 0,
+            TRUE ~ 1)) %>% 
+        select(nome, ano, Prio)
+    
+    mun_bioma <- 
+      Bioma_Predominante_por_Municipio_2024 %>%
+      rename(bioma = `Bioma predominante`, id_municipio = Geocódigo) %>%
+      select(id_municipio, bioma) %>%
+      mutate(id_municipio = as.character(id_municipio)) 
+    
+    left_join(soja_data, mun_bioma) %>%
+      filter(bioma == "Amazônia") %>% 
+      group_by(id_municipio)
     
     mun_aux <- 
       left_join(mun_bioamazonia, mun_prio) %>% 
@@ -81,6 +100,10 @@
         Prio = if_else(is.na(Prio), 0, Prio),
         amazonia = 1) %>%
       select(-nome)
+    
+    left_join(mun_bioamazonia, soja_data) %>%
+      filter(!is.na(ano)) %>%
+      group_by(id_municipio)
     
     df_out<-
       left_join(soja_data, mun_aux) %>% 
